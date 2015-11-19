64464b9aebe78557c9e87c0fbd97a27ce55d7ee5
Some Guy <abc@abc.com>
2015-11-19 09:24:46 -0800

    fix url for api

--- api/urls.py
+++ api/urls.py
@@ -3,10 +3,10 @@ from api.views import ListUsers, DetailUpdatePledge, DetailUpdateItem, \
 from django.conf.urls import url
 
 urlpatterns = [
-    url(r'^users/', ListUsers.as_view()),
-    url(r'^wishlists/', ListWishLists.as_view()),
-    url(r'^items/', ListItems.as_view()),
-    url(r'^pledges/', ListPledges.as_view()),
+    url(r'^users/$', ListUsers.as_view()),
+    url(r'^wishlists/$', ListWishLists.as_view()),
+    url(r'^items/$', ListItems.as_view()),
+    url(r'^pledges/$', ListPledges.as_view()),
     url(r'^wishlists/(?P<pk>\d+)', DetailUpdateWishList.as_view()),
     url(r'^items/(?P<pk>\d+)', DetailUpdateItem.as_view()),
     url(r'^pledges/(?P<pk>\d+)', DetailUpdatePledge.as_view()),
