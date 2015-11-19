abd70eebf93b90a3fca373a5564d1fa2c31ff75d
Some Guy <abc@abc.com>
2015-11-19 09:50:29 -0800

    +

--- api/serializers.py
+++ api/serializers.py
@@ -7,18 +7,21 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'email')
+        read_only_fields = ('id', )
 
 
 class WishListSerializer(serializers.ModelSerializer):
     class Meta:
         model = WishList
         fields = ('id', 'user', 'title', 'expiration', 'created_at')
+        read_only_fields = ('id', 'user', 'created_at')
 
 
 class PledgeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pledge
         fields = ('id', 'user', 'item', 'amount', 'pledged_at')
+        read_only_fields = ('id', 'user', 'pledged_at')
 
 
 class ItemSerializer(serializers.ModelSerializer):
@@ -29,3 +32,5 @@ class ItemSerializer(serializers.ModelSerializer):
         fields = ('id', 'wish_list', 'title', 'description', 'price',
                   'source_url', 'image_url', 'created_at', 'pledge_set',
                   'total_pledged', 'amount_needed')
+        read_only_fields = ('id', 'created_at', 'total_pledged',
+                            'amount_needed')
