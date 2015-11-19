f3428f8280f11d3a69f06645a0095da28fe831d3
Some Guy <abc@abc.com>
2015-11-19 13:44:41 -0800

    added user registration in api

--- api/serializers.py
+++ api/serializers.py
@@ -6,8 +6,19 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email')
+        fields = ('id', 'username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
         read_only_fields = ('id', )
+
+
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
 
 
 class WishListSerializer(serializers.ModelSerializer):
