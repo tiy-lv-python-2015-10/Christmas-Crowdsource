98eed3e3e6fd7787ac9aeeec3bee3a2e35e1b68b
cesarmarroquin <cesarm2333@gmail.com>
2015-11-19 08:56:34 -0800

    Fixing static again

--- christmas_crowdsource/heroku-settings.py
+++ christmas_crowdsource/heroku-settings.py
@@ -21,7 +21,3 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '/christmas_lists/static'),
-)
