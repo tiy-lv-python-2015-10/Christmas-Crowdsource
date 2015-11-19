dd2471ddbd995a49080d9982f2958e9c2a2b2a65
Some Guy <abc@abc.com>
2015-11-19 10:03:13 -0800

    add auth-token url

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from api.views import ListUsers, DetailUpdatePledge, DetailUpdateItem, \
     DetailUpdateWishList, ListPledges, ListItems, ListWishLists
 from django.conf.urls import url
+from rest_framework.authtoken import views
 
 urlpatterns = [
     url(r'^users/$', ListUsers.as_view()),
@@ -10,4 +11,5 @@ urlpatterns = [
     url(r'^wishlists/(?P<pk>\d+)', DetailUpdateWishList.as_view()),
     url(r'^items/(?P<pk>\d+)', DetailUpdateItem.as_view()),
     url(r'^pledges/(?P<pk>\d+)', DetailUpdatePledge.as_view()),
+    url(r'^api-token-auth/$', views.obtain_auth_token)
 ]
