bebcb2cc4cdaed7be88be42db9d44acb05261f7f
cesarmarroquin <cesarm2333@gmail.com>
2015-11-22 19:24:49 -0800

    added stripe logic to pledge view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,4 @@
+import os
 from api import permissions
 from christmas_lists.models import WishList, Item, Pledge
 from django.db.models import Count
@@ -8,6 +9,7 @@ from rest_framework import generics, permissions, filters
 from rest_framework.throttling import AnonRateThrottle
 from api.permissions import IsOwnerOrReadOnly
 from api.serializers import WishlistSerializer, ItemSerializer, PledgeSerializer
+import stripe
 
 
 class SmallPagination(PageNumberPagination):
@@ -69,9 +71,45 @@ class APIListCreatePledge(generics.ListCreateAPIView):
     filter_backends = (filters.SearchFilter,)
     # search_fields = ('posting_title', 'specific_location', 'posting_body')
 
+
     def perform_create(self, serializer):
-        user = self.request.user
-        serializer.save(user=user)
+        stripe.api_key = os.environ('STRIPE_API_KEY')
+        token = serializer.initial_data['token']
+
+        try:
+           charge = stripe.Charge.create(
+               amount=serializer.initial_data['amount'],
+               currency="usd",
+               source=token,
+               description="Pledge"
+           )
+           charge_id = charge['id']
+        except stripe.error.CardError:
+           pass
+
+        serializer.save()
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        username = self.request.query_params.get('username', None)
+        if username:
+           qs = qs.filter(user__username=username)
+        return qs
+
+
+
+
+
+
+
+
+
+
+
+
+    # def perform_create(self, serializer):
+    #     user = self.request.user
+    #     serializer.save(user=user)
 
 
 class APIDetailUpdatePledge(generics.RetrieveUpdateDestroyAPIView):
